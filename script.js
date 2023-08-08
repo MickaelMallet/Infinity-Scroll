e6b1d71fcd4ef5b992d2cdc86afdccb0523bf567
MickaelMallet <mickaelmallet7@gmail.com>
2023-08-08 10:21:42 +0200

    infinite scroll, DRY code

--- script.js
+++ script.js
@@ -1,44 +1,78 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
 //Unsplash API 
-const count = 10;
-const apiKey= 'l2Y2fs3FpIagmV3FA8HMMjGjaAano5xsBPt6_Iaslpw';
+const count = 30;
+const apiKey= 'EssczCOEBW0zuNzMDEE8WL_ZPEQmHIkHyTJ5z1Zq_HM';
 const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-// Creation des elements pour les photos et ajouts dans le DOM
-function displayPhotos() {
-    // On parcours le tableau des photos
-    photosArray.forEach((photo) => {
-        // On cree un element <a> pour lier vers Unsplash
-        const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
-        // On cree un element <img> pour les photos
-        const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-        // On ajoute l'element <img> dans l'element <a>
-        item.appendChild(img);
-        // On ajoute l'element <a> dans l'element <imageContainer>
-        imageContainer.appendChild(item);
-    });
 
-}
-
-// Recuperation des photos via l'api Unsplash
-async function getPhotos(){
-    try {
-        const response = await fetch(apiUrl);
-        photosArray = await response.json();
-        displayPhotos();
-    }catch (error){
-        // on recupere l'erreur
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+      ready = true;
+      loader.hidden = true;
     }
-}
-
-// On charge les photos
-getPhotos();
+  }
+  
+  // Helper Function to Set Attributes on DOM Elements
+  function setAttributes(element, attributes) {
+    for (const key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+  }
+  
+  // Create Elements For Links & Photos, Add to DOM
+  function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+      // Create <a> to link to full photo
+      const item = document.createElement('a');
+      setAttributes(item, {
+        href: photo.links.html,
+        target: '_blank',
+      });
+      // Create <img> for photo
+      const img = document.createElement('img');
+      setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+      });
+      // Event Listener, check when each is finished loading
+      img.addEventListener('load', imageLoaded);
+      // Put <img> inside <a>, then put both inside imageContainer Element
+      item.appendChild(img);
+      imageContainer.appendChild(item);
+    });
+  }
+  
+  // Get photos from Unsplash API
+  async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl);
+      photosArray = await response.json();
+      displayPhotos();
+    } catch (error) {
+      // Catch Error Here
+    }
+  }
+  
+  // Check to see if scrolling near bottom of page, Load More Photos
+  window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+      ready = false;
+      getPhotos();
+    }
+  });
+  
+  // On Load
+  getPhotos();
